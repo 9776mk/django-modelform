413e53a43be09fd7d1fb10e6de9eb36aa3b2f187
9776mk <9776mk@gmail.com>
2022-10-04 18:36:04 +0900

    08 - 게시글 목록

--- articles/views.py
+++ articles/views.py
@@ -5,8 +5,17 @@ from .models import Article
 
 # 요청 정보를 받아서
 def index(request):
+    # 게시글을 가져와서
+    # articles = Article.objects.all()
+    articles = Article.objects.order_by('-pk')
+    
+    # Template에 전달한다.
+    context = {
+        'articles': articles
+    }
+
     # 페이지를 render
-    return render(request,'articles/index.html')
+    return render(request,'articles/index.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
